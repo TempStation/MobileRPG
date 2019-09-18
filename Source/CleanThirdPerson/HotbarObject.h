7bf9dbee4c9070a97e54a0560111dd2affd28344
TempStation <tempstation.travik@gmail.com>
2019-09-18 10:43:06 +0300

    MobileRPG_001

---
+++ Source/CleanThirdPerson/HotbarObject.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/DataAsset.h"
+#include "HotbarObject.generated.h"
+
+class UTexture2D;
+
+UCLASS(Blueprintable, BlueprintType)
+class CLEANTHIRDPERSON_API UHotbarObject : public UDataAsset
+{
+	GENERATED_BODY()
+	
+	public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = General)
+	FText Name;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = General)
+	TAssetPtr<UTexture2D> Icon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = General)
+	FText Description;
+};
